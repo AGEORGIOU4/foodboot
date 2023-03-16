50f4a21bb0e617d19631c985c2d7d484a0aa0586
Andreas Georgiou <andreasg046@gmail.com>
2023-03-16 22:46:32 +0200

    Update Common.js

--- src/components/Common.js
+++ src/components/Common.js
@@ -1,4 +1,6 @@
-export const mainUrl = (window.location.hostname == 'localhost') ? 'http://localhost:5000' : 'https://foodboot-backend.herokuapp.com';
+// export const mainUrl = (window.location.hostname == 'localhost') ? 'http://localhost:5000' : 'https://foodboot-backend.herokuapp.com';
+export const mainUrl = (window.location.hostname == 'localhost') ? 'http://localhost:5000' : 'https://dev.silversky3d.com/api-foodboot';
+
 export const auth0APItoken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlVyRF9LdHljVHlQYUZTZFhPQ2h5bCJ9.eyJpc3MiOiJodHRwczovL2Zvb2Rib290LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJxeVRpVWZnYmNtQ3BJanBrZ1NaZEtUOUNXTmZSTVVBV0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9mb29kYm9vdC5ldS5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTY3ODQ1NDU2MiwiZXhwIjoxNjgxMDQ2NTYyLCJhenAiOiJxeVRpVWZnYmNtQ3BJanBrZ1NaZEtUOUNXTmZSTVVBVyIsInNjb3BlIjoicmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgdXBkYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.m9oSFhDxbi44zO6E-bfYpwh18pYCEcqUKoZXlf4ea6vi3jYUUZ6n8IuAfdQFMhLZvnzckdj2iBh_e3cTeTJmxpitoJBzPp1tVr1DThp0RqTgW46sG7zZRwTQ-7KkD0ROh8GtgAEUEPQIKQOjscUpm3qeqiHASil8eQU6ZBdwSPto-wbIb2Nr67r-STcukbFEKbqVl4L0DoDp8QiOLuw7luPIYudvjj0z7a31mI4sdCFRyMJfcBh8EhtC85twbtCNrZ6y6DdWgqBVR5GaceVRlPcIGm9GIKytwZuME9P1jVK6d4zKLKNsWOAhVbUylzoFZDkFWoQzOyzI8FSvTC-siA';
 
 console.log("Fetching data from " + mainUrl);
